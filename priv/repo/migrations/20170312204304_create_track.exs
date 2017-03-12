fa9718fc1c7ab9e4b94854251ce0edc5c1a76845
Eddy Lane <naedin@gmail.com>
2017-03-12 21:16:01 +0000

    Added track model

---
+++ priv/repo/migrations/20170312204304_create_track.exs
@@ -0,0 +1,14 @@
+defmodule ElixirElmBootstrap.Repo.Migrations.CreateTrack do
+  use Ecto.Migration
+
+  def change do
+    create table(:tracks) do
+      add :spotify_id, :string
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+    create index(:tracks, [:user_id])
+
+  end
+end
--- web/models/user.ex
+++ web/models/user.ex
@@ -9,6 +9,8 @@ defmodule ElixirElmBootstrap.User do
     field :password, :string, virtual: true
     field :password_hash, :string
 
+    has_many :tracks, ElixirElmBootstrap.Track
+
     timestamps
   end
 
